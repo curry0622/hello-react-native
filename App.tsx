bf55460e7a7dbda4835706f0bac565782f64bc19
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-27 16:51:01 +0800

    feat: flatlist practice

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ListRenderItem } from 'react-native';
+
+interface IUser {
+  acc: string;
+  psw: string;
+}
 
 export default function App() {
+  const userArr: IUser[] = [
+    {
+      acc: 'curry0622',
+      psw: '24213123'
+    },
+    {
+      acc: 'naiyun0203',
+      psw: '2134'
+    },
+    {
+      acc: 'jacky0522',
+      psw: '214567'
+    },
+  ];
+
+  const renderUser:ListRenderItem<IUser> = ({ item }) => (
+    <View>
+      <Text>{item.acc}</Text>
+      <Text>{item.psw}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={userArr}
+        renderItem={renderUser}
+      />
     </View>
   );
 }
